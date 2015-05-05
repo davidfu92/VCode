e99fc1e8dfa0910845373fd764d46cf55c00ef0f
Jan <jan.anthony.m@gmail.com>
2015-05-05 12:17:52 -0400

    add client

---
+++ client.c
@@ -0,0 +1,10 @@
+#include 	<stdio.h>
+#include    <unistd.h>
+#include	<sys/socket.h>	/* basic socket definitions */
+#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+#include	<arpa/inet.h>	/* inet(3) functions */
+#include	<errno.h>
+#include	<string.h>
+#include    <sys/select.h>
+#include	<sys/time.h>
+#include <ncurses.h>
